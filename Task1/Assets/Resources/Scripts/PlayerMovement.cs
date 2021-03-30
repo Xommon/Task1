92ab3c0714c1cfac862d9dbcf2901f22101e1558
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 11:47:31 -0500

    Player Movement
    The selected player can move with WASD.

--- Task1/Assets/Resources/Scripts/PlayerMovement.cs
+++ Task1/Assets/Resources/Scripts/PlayerMovement.cs
@@ -7,13 +7,16 @@ public class PlayerMovement : MonoBehaviour
 {
     public bool selected;
     public Color startingColour;
-    public Renderer renderer;
+    public Renderer renderer; 
+    public Rigidbody rb;
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get references
         renderer = GetComponent<Renderer>();
+        rb = GetComponent<Rigidbody>();
 
         // Define starting variables
         startingColour = renderer.material.color;
@@ -22,7 +25,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (selected)
+        {
+            // Move the selected player
+            rb.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        }
     }
 
     private void OnMouseDown()
