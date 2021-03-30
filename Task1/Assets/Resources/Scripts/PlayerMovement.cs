4c90d8361e36373e800c72ab31b27da22d4cc8f7
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 11:43:35 -0500

    Players Selectable
    The user can click on the cube they want to select, and the selected cube highlights in red.

---
+++ Task1/Assets/Resources/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public bool selected;
+    public Color startingColour;
+    public Renderer renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get references
+        renderer = GetComponent<Renderer>();
+
+        // Define starting variables
+        startingColour = renderer.material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        foreach (PlayerMovement player in FindObjectsOfType<PlayerMovement>())
+        {
+            // Unselect other players
+            player.selected = false;
+            player.renderer.material.color = player.startingColour;
+        }
+
+        selected = true;
+        renderer.material.color = Color.red;
+    }
+}
