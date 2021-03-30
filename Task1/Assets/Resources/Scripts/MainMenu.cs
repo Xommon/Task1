2cbe2574d6ca6ee7ec470937fd83a1ed8a11911d
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 11:28:43 -0500

    Main Menu Coded
    The user can now move to the game scene with the play button or close the application with the exit button.

---
+++ Task1/Assets/Resources/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        // Move to the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitButton()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
