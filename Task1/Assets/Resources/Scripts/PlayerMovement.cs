74d01450b610d3b501a3e73b2bf196b0de872ca9
Micheal Quentin <mquenti3@gmail.com>
2021-03-30 11:52:36 -0500

    Cubes Respawn
    Cubes will return to their starting positions if they fall off of the edge after 7 seconds.

--- Task1/Assets/Resources/Scripts/PlayerMovement.cs
+++ Task1/Assets/Resources/Scripts/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public bool selected;
     public Color startingColour;
+    public Vector3 startingPosition;
     public Renderer renderer; 
     public Rigidbody rb;
     public float speed;
+    public float timer;
+    public bool onGround;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,7 @@ public class PlayerMovement : MonoBehaviour
 
         // Define starting variables
         startingColour = renderer.material.color;
+        startingPosition = rb.position;
     }
 
     // Update is called once per frame
@@ -29,6 +33,23 @@ public class PlayerMovement : MonoBehaviour
         {
             // Move the selected player
             rb.position += new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        }
+
+        if (!onGround)
+        {
+            // Decrease the timer is the player has fallen off
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            timer = 7.0f;
+        }
+
+        if (timer <= 0)
+        {
+            // Return the player to the starting position
+            rb.position = startingPosition;
+            rb.rotation = Quaternion.identity;
         }
     }
 
@@ -43,5 +64,23 @@ public class PlayerMovement : MonoBehaviour
 
         selected = true;
         renderer.material.color = Color.red;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            // The player is on the ground as long as it touches the ground object
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            // The player has fallen off of the edge of the map
+            onGround = false;
+        }
     }
 }
